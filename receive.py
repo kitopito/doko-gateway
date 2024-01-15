c40b3447c6b5c7f6151c48439f2fe75ef8df6f8d
kitopito <kitopitowada@gmail.com>
2024-01-15 12:40:35 +0900

    worker

--- receive.py
+++ receive.py
@@ -7,7 +7,10 @@ import datetime
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
-  
+
+import requests
+   
+workerURL = "https://dokosen-worker.kitopitowada.workers.dev/sensors"
 # Use a service account
 cred = credentials.Certificate('dokosen-firebase-adminsdk.json')
 firebase_admin.initialize_app(cred)
@@ -87,26 +90,67 @@ def main():
                         #print(payload.hex())
                         payload_hex = payload.hex()
                         #k = payload_hex[0]*16 + payload_hex[1]
+                        '''
                         first = int(payload_hex[0]) * 16 + int(payload_hex[1])
                         second = int(payload_hex[2], 16) * 16 + int(payload_hex[3], 16)
                         third = int(payload_hex[4], 16) * 16 + int(payload_hex[5], 16)
-                        if first == int(0x55) and third == int(0x64):
+                        '''
+
+                        message_length = len(payload_hex)
+                        #print("message length " + str(message_length))
+                        dataNum = int((message_length / 2 - 12) / 2)
+                        #print("data num " + str(dataNum))
+
+                        payload_byte = list()
+                        for i in range(message_length//2):
+                            payload_byte.append(int(payload_hex[i * 2], 16) * 16 + int(payload_hex[i * 2 + 1], 16))
+                            #print(payload_byte[i])
+
+                        payload_log = ''
+                        for i in range(message_length):
+                            payload_log += str(payload_hex[i])
+                        print(payload_log)
+
+
+                        isEnoughLength = len(payload_byte) > 12
+                        isValidData = isEnoughLength;
+                        if isEnoughLength:
+                            isMine = payload_byte[0] == 0
+                            isIYH = payload_byte[2] == ord('i')
+                            isSTX = payload_byte[8] == ord('U')
+                            isData = payload_byte[10] == ord('d')
+                            isEnd = payload_byte[11 + 2 * dataNum] == ord('^')
+                            isValidData = isMine and isIYH and isSTX and isData and isEnd
+                        print(isValidData)
+                        isValidDataNum = 0
+                        if isValidData:
+                            isValidDataNum = 1
+
+                        values = list()
+                        dataHead = 11
+                        if isValidData:
                             #print('hoge')
-                            value = [0, 0, 0, ]
-                            for i in range(3):
-                                for j in range(4):
-                                    value[i] += int(payload_hex[6 + 4*i + j], 16) * 16 ** (3 - j)
+                            for i in range(dataNum):
+                                value = payload_byte[dataHead + i * 2] * 256 + payload_byte[dataHead + i * 2 + 1]
+                                values.append(value)
+                                print(value)
                             #value1 = int(payload_hex[3] * 256 + payload_hex[4])
                             #value2 = int(payload_hex[5] * 256 + payload_hex[6])
                             #value3 = int(payload_hex[7] * 256 + payload_hex[8])
                             #print(value[0])
                             #print(value[1])
                             #print(value[2])
+                            '''
                             status_number_kari = value2State(value[0], value[1], value[2]) #int(str_d[-1])
                             print(status_number_kari)
                             if status_number != status_number_kari:
                                 status_number = status_number_kari
                                 updateData(status_number)
+                            '''
+
+                            deviceAddress = payload_byte[9]
+                            j = {"deviceAddress": deviceAddress, "value": values, "log": payload_log, isValidData: isValidDataNum}
+                            requests.post(workerURL, json = j)
 
                         payload = bytes()
     else:
